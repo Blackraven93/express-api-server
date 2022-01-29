ae767742378a266fe653236ea839bfa4f7da89f3
kwak se jin <Reblackraven@gmail.com>
2022-01-29 22:37:27 +0900

    Refactor: refactor singleton & service patton

--- src/app.ts
+++ src/app.ts
@@ -4,30 +4,56 @@ import 'dotenv/config'
 import { Birds, BirdType } from "./routers/birds/birds.model";
 import birdRouter from "./routers/birds/birds.router";
 
-const app: express.Express = express();
-const { PORT } = process.env || 2300
-
-console.log("Express + typescript")
-
-//* logging middleware
-app.use((req: express.Request, res: express.Response, next) => {
-    console.log(req.rawHeaders[1])
-    console.log("This is logging middleware")
-    next()
-})
-
-//* json middleware
-app.use(express.json())
-
-app.use(birdRouter);
-
-//* 404 middleware
-app.use((req: express.Request, res: express.Response, next) => {
-    res.status(400).send({ error: "404 not found error" })
-})
 
 
-// init
-app.listen(PORT, () => {
-    console.log(`Listening http://localhost:${PORT}`)
-})
+class Server {
+    public app: express.Application;
+    private PORT: undefined | string
+
+
+    constructor() {
+        const { PORT } = process.env || 2300
+        const app: express.Application = express();
+        this.app = app;
+        this.PORT = PORT;
+    }
+
+    private setRoute() {
+        this.app.use(birdRouter);
+    }
+
+    private setMiddleware() {
+        //* logging middleware
+        this.app.use((req: express.Request, res: express.Response, next) => {
+            console.log(req.rawHeaders[1])
+            console.log("This is logging middleware")
+            next()
+        })
+
+        //* json middleware
+        this.app.use(express.json())
+
+        // router
+        this.setRoute()
+
+        //* 404 middleware
+        this.app.use((req: express.Request, res: express.Response, next) => {
+            res.status(400).send({ error: "404 not found error" })
+        })
+    }
+
+    public listen() {
+        this.setMiddleware()
+        this.app.listen(this.PORT, () => {
+            console.log(`Listening http://localhost:${this.PORT}`)
+        })
+    }
+}
+
+
+const init = () => {
+    const server = new Server()
+    server.listen()
+}
+
+init()

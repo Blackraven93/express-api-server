4e1c2a4365f5b981bb73e11c09b8b55e79186fd8
kwak se jin <Reblackraven@gmail.com>
2022-01-29 21:01:35 +0900

    Feat: add get, post function about sample data

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import * as express from "express";
 import 'dotenv/config'
 // import dotenv from "dotenv" 차이가 있음
 
-
 import { Birds, BirdType } from "./app.model";
 
 const app: express.Express = express();
@@ -10,30 +9,70 @@ const { PORT } = process.env || 2300
 
 console.log("Express + typescript")
 
-// logging middleware
+//* logging middleware
 app.use((req: express.Request, res: express.Response, next) => {
     console.log(req.rawHeaders[1])
     console.log("This is logging middleware")
     next()
 })
 
-// Router
+//* json middleware
+app.use(express.json())
+
+//* Router
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send({ Birds })
+    res.status(200).send("<h1>I Like bird</h1>")
 })
+
+//* Read Bird 전체 데이터 모두 조회
 
 app.get("/birds", (req, res) => {
-    res.send({ Birds, raven: Birds[0] })
+    try {
+        res.status(200).send({
+            success: true,
+            Birds
+        });
+    } catch (err) {
+        throw new Error("Can't found birds data")
+    }
 })
 
-app.post("/test", (req: express.Request, res: express.Response) => {
-    //
+//* Read Bird 특정 데이터 조회
+
+app.get("/birds/:id(\\d+)", (req: express.Request, res: express.Response) => {
+    const { id } = req.params
+    const lastIndex = Birds.length - 1
+    if (Birds[lastIndex]["id"] < Number(id)) {
+        res.status(400).send({ error: "404 not found error" })
+    }
+    try {
+        const bird = Birds.find(e => {
+            if (e.id === Number(id)) return e
+        })
+        res.status(200).send({ bird })
+    } catch (error) {
+        throw new Error("Can't found anything")
+    }
 })
 
+//* Create Bird 특정 데이터 추가
 
-// 404 middleware
+app.post("/birds", (req: express.Request, res: express.Response) => {
+    const data = req.body
+    Birds.push(data)
+    try {
+        res.status(200).send({
+            success: true,
+            Birds
+        });
+    } catch (err) {
+        throw new Error("Can't found birds data")
+    }
+})
+
+//* 404 middleware
 app.use((req: express.Request, res: express.Response, next) => {
-    res.send({ error: "404 not found error" })
+    res.status(400).send({ error: "404 not found error" })
 })
 
 
